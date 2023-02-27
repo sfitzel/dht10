2c6bf8a23fd3184371aae4c73ce79d2c5ef57616
sfitzel <sfitzel@hotmail.com>
2023-02-26 17:04:11 -0700

    Update 1

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+const config = {
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		"./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
+		"./node_modules/flowbite-svelte-blocks/**/*.{html,js,svelte,ts}",
+	],
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: [
+		require('flowbite/plugin')
+	],
+	darkmode:'class',
+};
+
+module.exports = config;
